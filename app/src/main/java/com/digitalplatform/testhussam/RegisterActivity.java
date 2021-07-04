aa54bee64c458da6647a86748bfea1c78e93aab0
Mr.Amira <hussam.amira95@gmail.com>
2021-07-04 23:13:40 +0300

    Add Register Activity

---
+++ app/src/main/java/com/digitalplatform/testhussam/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.digitalplatform.testhussam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //Comment 101
+    }
+}
